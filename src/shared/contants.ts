3825461d348d1bf0a12f678eb8e317fed0000de5
toxac <derocon@gmail.com>
2024-01-15 19:51:45 +0530

    app contants - nav links

---
+++ src/shared/contants.ts
@@ -0,0 +1,11 @@
+interface NavLinks {
+  label: string;
+  href: string;
+}
+
+export const mainNavLinks: NavLinks[] = [
+  { href: "#home", label: "Home" },
+  { href: "#about-us", label: "About Us" },
+  { href: "#products", label: "Products" },
+  { href: "#contact-us", label: "Contact Us" },
+];
